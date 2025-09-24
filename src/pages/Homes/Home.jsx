333cc241aed1a95a8330f6589c887c73ebcb5280
Busra2456 <you@example.com>
2025-09-24 18:40:47 +0600

    NavBar footer Banner Category

---
+++ src/pages/Homes/Home.jsx
@@ -0,0 +1,13 @@
+import Banner from "../Home/Banner/Banner";
+import Category from "../Home/Category/Category";
+
+const Home = () => {
+      return (
+            <div>
+                  <Banner></Banner>
+                  <Category></Category>
+            </div>
+      );
+};
+
+export default Home;
