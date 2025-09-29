25563fd7dfa4050b09ce666cb26decae44f9432c
Busra2456 <you@example.com>
2025-09-29 15:56:43 +0600

    Shared Section Title For Different Sections

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -10,11 +10,17 @@ import slide2 from '../../../assets/home/slide2.jpg';
 import slide3 from '../../../assets/home/slide3.jpg';
 import slide4 from '../../../assets/home/slide4.jpg';
 import slide5 from '../../../assets/home/slide5.jpg';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 
 
 const Category = () => {
       return (
-            <div>
+            
+              <section>
+                <SectionTitle
+                subHeading={"From 11.00am to 10.00pm"}
+                heading={"Order Online"}
+                ></SectionTitle>
                    <Swiper
         slidesPerView={3}
         spaceBetween={30}
@@ -37,8 +43,8 @@ const Category = () => {
         <h3 className='text-4xl uppercase text-center -mt-20 text-white'>Salads</h3></SwiperSlide>
        
       </Swiper>
-                  
-            </div>
+              </section>
+                   
       );
 };
 
